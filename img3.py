580feda0248b5f23b24439756dedcf24b216db5f
deng3h <3hdeng@gmail.com>
2016-07-07 08:54:44 +0000

    need matplotlib for plt.show(), need PIL for Image

--- img3.py
+++ img3.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
 
 filenames = ['brain_left_right.jpg']
 filename_queue = tf.train.string_input_producer(filenames)
@@ -9,20 +12,27 @@ key, value = reader.read(filename_queue)
 
 images = tf.image.decode_jpeg(value, channels=3)
 
+sess = tf.InteractiveSession()
 init_op = tf.initialize_all_variables()
-with tf.Session() as sess:  #sess.as_default():
+#with tf.Session() as sess.as_default():
+with sess.as_default():
   sess.run(init_op)
 
 # Start populating the filename queue.
 
 coord = tf.train.Coordinator()
-threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+threads = tf.train.start_queue_runners(coord=coord) #, sess=sess)
 
 for i in range(len(filenames)): #length of your filename list
-  image = images.eval(session=sess) #here is your image Tensor :) 
+  image = images.eval() #here is your image Tensor :) 
 
 print(image.shape)
-Image.show(Image.fromarray(np.asarray(image)))
+plt.show(Image.fromarray(np.asarray(image)))
 
 coord.request_stop()
 coord.join(threads)
+
+"""
+raise RuntimeError('Attempted to use a closed Session.')
+
+"""
