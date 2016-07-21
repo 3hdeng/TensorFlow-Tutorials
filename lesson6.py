06fada1738e8a678f1b21cf65f54ef9c5a3754b1
deng3h <3hdeng@gmail.com>
2016-07-21 07:44:53 +0000

    tricky function chain mapping in tensorflow, how to tell value or function is used?

--- lesson6.py
+++ lesson6.py
@@ -39,25 +39,30 @@ seed2=310
 data_centroids, samples = create_samples(n_clusters, n_samples_per_cluster, n_features, embiggen_factor, seed)
 initial_centroids = choose_random_centroids(samples, n_clusters,310)
 nearest_indices = assign_to_nearest(samples, initial_centroids)
+# nearest_indices = assign_to_nearest(samples, updated_centroids)
 updated_centroids = update_centroids(samples, nearest_indices, n_clusters)
 diff= tf.sub(updated_centroids, data_centroids)
 distance = tf.reduce_sum( tf.square(diff))
 threshold= tf.constant(1.5)
 
 model = tf.initialize_all_variables()
+# tricky recursive function chain mapping onto tf
 with tf.Session() as session:
-    sample_values = session.run(samples)
-    centroid_values= session.run(data_centroids)
+    # sample_values = session.run(samples)
+    # centroid_values= session.run(data_centroids)
     print(centroid_values)
     for i in range(1000) :
-      if session.run(tf.less(distance, threshold)):
-        print("distance lower than th, break on ", i)  
-        break
-      else:
+      #if session.run(tf.less(distance, threshold)):
+      #  print("distance lower than th, break on ", i)  
+      #  break
+      #else:
         [updated_centroids_value , distance_value]= session.run([updated_centroids, distance])
         # distance_value=session.run(distance)
         #print(updated_centroids_value)
         print(distance_value)
-        nearest_indices = assign_to_nearest(samples, updated_centroids)
-
+        # nearest_indices = assign_to_nearest(samples, updated_centroids)
+        if(distance_value < 1.5):
+            print(distance_value,", distance lower than th, break on ", i)  
+            print(updated_centroids_value, session.run(data_centroids))
+            break
 
