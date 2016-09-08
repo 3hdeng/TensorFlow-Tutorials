1c3c6a83f563bddbd2397902394d07a8741b8a82
deng3h <3hdeng@gmail.com>
2016-09-08 04:02:38 +0000

    return [x,y] not allowed ?  Fetch argumen ... has invalid type <type 'list'>, must be a string or Tensor. (Can not convert a list into a Tensor or Operation.)

--- demo_conv0.py
+++ demo_conv0.py
@@ -35,7 +35,9 @@ def model(X, w, w2, w3, w4, w_o):
     #saver = tf.train.Saver({"my_Xval": X[0,:, :, 1], "my_L1a":l1a[0, :, :, 15]})
     #save_path = saver.save(sess, "tmp/myVars.ckpt") 
     # print("Model saved in file: %s" % save_path)
-    return pyx, l1a #X[0,:, :, 1], [l1a[0, :, :,0] , l1a[0, :, :, 15] ]
+    l1a_0=tf.slice(l1a,[0,0,0,0], [1,-1, -1, 1]) # tf.slice(arr, begin, size)
+    l1a_15=tf.slice(l1a,[0,0,0,0], [1,-1, -1, 15])
+    return pyx,  [l1a_0, l1a_15] # xxxx [l1a[0, :, :,0] , l1a[0, :, :, 15] ]
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
@@ -94,12 +96,12 @@ for i in range(100):
             result=sess.run([py_x, L1a_sample], feed_dict={X: trX[start:end], Y: trY[start:end] })
             # xxx X_val= sess.run(X[0,:,:,1].eval(session=sess) # X.eval(session=sess, feed_dict={X: trX[start:end]})[0,:,:,1]
             X_val=trX[start][:, :, 0]
-            L1a_val=result[1][0,:,:,0:1]
+            L1a_val=result[1][1] #[0,:,:,0:1]
             #train_val=sess.run(train_op)
             #result=sess.run([train_op, X_sample, L1a_sample], feed_dict={X: trX[start:end], Y: trY[start:end] })
             #train_val, X_val, L1a_val
-            print type(X_val)
-            print X_val.shape
+            print type(L1a_val)
+            print L1a_val.shape
             myplot(X_val, L1a_val, start) 
             
         test_indices = np.arange(len(teX)) # Get A Test Batch
